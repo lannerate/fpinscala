e45bf732b0c3a7c77a578010e1a32aa39b49b727
hui <zhanghuizaizhe@hotmail.com>
2016-09-27 22:48:46 +0800

    add java8 functional programming examples

--- answers/src/main/scala/fpinscala/datastructures/List.scala
+++ answers/src/main/scala/fpinscala/datastructures/List.scala
@@ -79,7 +79,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   /*
   Again, it's somewhat subjective whether to throw an exception when asked to drop more elements than the list
-  contains. The usual default for `drop` is not to throw an exception, since it's typically used in cases where this
+  contains. The usual defaultmethod for `drop` is not to throw an exception, since it's typically used in cases where this
   is not indicative of a programming error. If you pay attention to how you use `drop`, it's often in cases where the
   length of the input list is unknown, and the number of elements to be dropped is being computed from something else.
   If `drop` threw an exception, we'd have to first compute or check the length and only drop up to that many elements.
