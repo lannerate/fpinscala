03d0f2a3e10f1c08e27fbe8d9b987e86882747b0
hzhang3 <zhanghuizaizheli@hotmail.com>
2016-09-28 16:01:49 +0800

    add polymorphic functions: abstracting over types

--- exercises/src/main/scala/fp/gettingstarted/GetStarted.scala
+++ exercises/src/main/scala/fp/gettingstarted/GetStarted.scala
@@ -32,4 +32,63 @@ object GetStarted {
       else loop(n - 1, cur, prev + cur)
     loop(n, 0, 1)
   }
+
+  def main(args: Array[String]): Unit = {
+    println( formatResult("factorial(1)",1,factorial) )
+    println( formatResult("increment1",4, (x:Int) => x + 1 ) )
+    println( formatResult("increment2",4, (x) => x + 2 ) )
+    println( formatResult("increment3",4,  x => x + 3 ) )
+    println( formatResult("increment4",4,  _ + 4 ) )
+    println( formatResult("increment5",4,  x => { val r = x + 5; r}) )
+
+    val b = lessThan.apply(12,30);
+  }
+
+  def formatResult(name:String, n:Int, f: Int => Int ) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name,n,f(n))
+  }
+
+  val lessThan = new Function2[Int,Int,Boolean] {
+    override def apply(v1: Int, v2: Int): Boolean = v1 < v2
+  }
 }
+
+object PolymorphicBinarySearch {
+
+  def binarySearch[A](as: Array[A], key: A, gt: (A, A) => Boolean): Int = {
+    @tailrec
+    def go(low: Int, mid: Int, high: Int): Int = {
+      if (low > high) -mid - 1
+      else {
+        val mid2 = (low + high) / 2
+        val a = as(mid2)
+        val greater = gt(a, key)
+        if (!greater && !gt(key, a)) mid2
+        else if (greater) go(low, mid2, mid2 - 1)
+        else go(mid2 + 1, mid2, high)
+      }
+    }
+    go(0, 0, as.length - 1)
+  }
+
+}
+
+object MonomorphicBinarySearch{
+
+  def binarySearch(as:Array[Double], key:Double):Int = {
+    @tailrec
+    def go(low:Int,mid:Int,high:Int):Int = {
+      if(low > high) -mid - 1
+      else{
+        val mid2 = (low + high)/2
+        val d = as(mid2)
+        if (d == key) mid2
+        else if(d > key) go(low,mid2,mid2-1)
+        else go(mid2+1, mid2, high)
+      }
+    }
+    go(0,0,as.length-1)
+  }
+}
+
--- exercises/src/test/scala/fpinscala/GetStartedTest.scala
+++ exercises/src/test/scala/fpinscala/GetStartedTest.scala
@@ -2,6 +2,7 @@ package fpinscala
 
 import org.scalatest.FlatSpec
 import fp.gettingstarted.GetStarted._
+import fp.gettingstarted.{MonomorphicBinarySearch, PolymorphicBinarySearch}
 
 /**
   * Created by apple on 9/23/16.
@@ -18,12 +19,36 @@ class GetStartedTest extends FlatSpec{
     assert( fib(6) == ( ( 2+1 ) + ( 1 +1 ) ) + ( 2 + 1 ) )
     assert( fib(7) == fib(6) + fib(5) )
   }
-
-  "Factorial function" should " satisfy f(n) = n! = n*(n-1)*(n-1)*...3*2*1 " in {
+"Factorial function" should " satisfy f(n) = n! = n*(n-1)*(n-1)*...3*2*1 " in {
     assume(factorial(1) == 1)
     assume(factorial(2) == 2*1)
     assume(factorial(3) == 3*2*1)
     assume(factorial(4) == 4*3*2*1)
     assume(factorial(5) == 5*4*3*2*1)
+  }
+
+  "Print test result" should " factorial(n) = n! " in {
+    assertResult("The factorial(n) of 1 is 1")(formatResult("factorial(n)",1, factorial ) );
+    assertResult("The factorial(n) of 2 is 2")(formatResult("factorial(n)",2, factorial ) );
+    assertResult("The factorial(n) of 3 is 6")(formatResult("factorial(n)",3, factorial ) );
+    assertResult("The factorial(n) of 4 is 24")(formatResult("factorial(n)",4, factorial ) );
+    assertResult("The factorial(n) of 5 is 120")(formatResult("factorial(n)",5, factorial ) );
+  }
+
+  "Less than function " should " 12 < 20 " in {
+    assert(lessThan(12,20))
+    assert(lessThan(11,20))
+  }
+
+  "Monomorphic Binary Search 99.3" should " at index(2) " in {
+    assertResult(2)(MonomorphicBinarySearch.binarySearch(Array(23.23,33.56,99.3,102.2,75.3,90.8),99.3))
+  }
+
+  "Polymorphic Binary Search 99.3" should " at index(2) " in {
+     val result = PolymorphicBinarySearch.binarySearch(
+      Array(23.23,33.56,99.3,102.2,75.3,90.8),
+      99.3,
+      (d1:Double,d2:Double) => d1 > d2)
+    assert( result == 2 )
   }
 }
