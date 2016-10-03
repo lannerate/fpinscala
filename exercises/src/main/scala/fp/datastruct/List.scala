45844ec6854c588a1c7af381027b29e9c8e72c64
hui <zhanghuizaizhe@hotmail.com>
2016-10-03 14:31:53 +0800

    implement the method of drop() and tail() for List

--- exercises/src/main/scala/fp/datastruct/List.scala
+++ exercises/src/main/scala/fp/datastruct/List.scala
@@ -14,12 +14,33 @@ object List {
     case Cons(x,xs) => x + sum(xs)
   }
 
+  def product(ds:List[Double]):Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+
   def apply[A](as: A*): List[A] =
     if(as.isEmpty) Nil
     else Cons(as.head,apply(as.tail: _*))
 
-  val example = List(1,2,3)
+  def tail[A](as: List[A]):List[A] =
+    as match {
+      case Nil => sys.error("tail is empty")
+      case Cons(_, t) => t
+    }
 
+  def drop[A](l:List[A], n:Int):List[A] = {
+    if(n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t,n-1)
+    }
+  }
+
+
+  val example = List(1,2,3)
+  val another = Cons(6, Cons(5,Cons(4,Cons(3,Cons(2,Cons(1,Nil))))))
   val total = sum(example);
 
 }
--- exercises/src/test/scala/fpinscala/ListTest.scala
+++ exercises/src/test/scala/fpinscala/ListTest.scala
@@ -13,6 +13,31 @@ class ListTest extends FunSpec{
       assert( List.sum( List(1,2,3,4,5) ) == 15 )
     }
 
+
+    it("list(1,2,3,4,5) by matching should return 3"){
+      val x = List(1,2,3,4,5) match {
+        case Cons(x, Cons(2, Cons(4, _))) => x
+        case Nil => 42
+        case Cons(x,Cons(y,Cons(3,Cons(4, _)))) => x + y
+        case Cons(h,t) => h + List.sum(t)
+        case _ => 101
+      }
+      assert(x == 3)
+    }
+
+    it("test tail implementation when List.trail(List(1,2,3,4)) should return Cons(2,Cons(3,Cons(4,Nil))"){
+      val x = List.tail(List(1,2,3,4));
+      val expected = Cons(2,Cons(3,Cons(4,Nil)))
+
+      assert( x == expected )
+    }
+
+    it("List.drop(List(1,2,3,4,5,6),2) should return Cons(3,Cons(4,Cons(5,Cons(6,Nil))))"){
+      val x = List.drop(List(1,2,3,4,5,6),2)
+      val expected = Cons(3,Cons(4,Cons(5,Cons(6,Nil))))
+
+      assert( x == expected )
+    }
   }
 
 }
