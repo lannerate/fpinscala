b6f71a81160536036664e57a9696c24248558944
hui <zhanghuizaizhe@hotmail.com>
2016-10-12 20:51:51 +0800

    implement maximum() for Tree[Int]

--- exercises/src/test/scala/fpinscala/TreeTest.scala
+++ exercises/src/test/scala/fpinscala/TreeTest.scala
@@ -6,16 +6,34 @@ import org.scalatest.FunSpec
 /**
   * Created by hzhang3 on 10/12/2016.
   */
-class TreeTest extends FunSpec{
+class TreeTest extends FunSpec {
 
-  describe("Test Tree cases"){
+  describe("Test Tree cases") {
 
-    it("the functional size() of branch(leaf,leaf)"){
-     val l = Leaf(1); val r = Leaf(2) ; val branch = Branch(l,r)
+    def mockData: Branch[Int] = {
+      val l = Leaf(1)
+      val r = Leaf(2)
+      val branch1 = Branch(l, r)
 
-     val size = Tree.size( branch )
+      val l2 = Leaf(3)
+      val r2 = Leaf(4)
+      val branch2 = Branch(l2, r2)
 
-     assert(size == 3)
+      Branch(branch1, branch2)
+    }
+
+    it("test size() of branch(branch1,branch2)") {
+
+      val size = Tree.size(mockData)
+
+      assert(size == 7)
+
+    }
+
+    it("test maximum() for branch(branch1,branch2)"){
+      val max = Tree.maximum(mockData)
+      assert(max == 4)
+
     }
   }
 }
